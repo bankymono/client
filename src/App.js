583fba80b4d54f871cc57171e35088b9b7f6a0f0
bankymono <bankymono@gmail.com>
2020-08-19 10:14:59 +0100

    netlify test 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import  {Provider } from 'react-redux'
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
+import store from './redux/store'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Route exact path='/' component={Home} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/dashboard' component={Dashboard} />
+        </Router>
+      </Provider>
     </div>
   );
 }
